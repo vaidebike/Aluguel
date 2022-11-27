bb0a48285ed0db3af9e1363716efc87f2cd4b7c5
cassianojr <cassiano.jr.2009@hotmail.com>
2022-11-27 16:54:52 -0300

    sonar token

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app =  express();
 const port =  process.env.PORT  ||  5000;
 // Route to be tested
 app.get('/', (req, res) => {
-    return res.status(200).json({ helloWorld:  'Olá mundo' });
+    return res.status(200).json({ nome:  'Olá mundo' });
 });
 // Application running on the door
 let server = app.listen(port, () => {
