fad16c4d02afe39d13ddb7b5bc5c3778ef2fe64d
cassianojr <cassiano.jr.2009@hotmail.com>
2022-11-28 11:36:53 -0300

    feat: created cyclist model and test

---
+++ src/models/Cyclist.ts
@@ -0,0 +1,21 @@
+export class Cyclist {
+  id: string;
+  status: StatusEnum;
+  name: string;
+  nascimento: Date;
+  cpf: string;
+  passaporte:{
+    number: string;
+    expiration: Date;
+    contry: string;
+  };
+  nationality: string;
+  email: string;
+  urlDocumentPhoto: string;
+}
+
+export enum StatusEnum {
+  Active = 'ATIVO',
+  Inactive = 'INATIVO',
+  WatingConfirmation = 'AGUARDANDO_CONFIRMACAO'
+}
