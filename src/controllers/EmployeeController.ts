d67d4da520e5d2606bfc0150d881b5e2cf1adc58
cassianojr <cassiano.jr.2009@hotmail.com>
2022-12-13 14:58:05 -0300

    feature: update and delete operations for employee

--- src/controllers/EmployeeController.ts
+++ src/controllers/EmployeeController.ts
@@ -4,25 +4,25 @@ import { NotFoundError } from '../errors/NotFoundError';
 import { NotValidError } from '../errors/NotValidError';
 import { EmployeeRepository } from '../models/repositories/EmployeeRepository';
 
-export class EmployeeController{
+export class EmployeeController {
 
   /**
    * Get one employee by id
    * @Route GET /employee/:id
    * @returns  Employee if found, otherwise 404
    */
-  public static async getOne(req: Request, res: Response){
-    const {id} = req.params;
-    
+  public static async getOne(req: Request, res: Response) {
+    const { id } = req.params;
+
     try {
       const cyclist = await new EmployeeRepository(req.app.get('db')).findOne(id);
       res.status(200).send(cyclist);
     } catch (error) {
       let status = 400;
-      if(error instanceof NotFoundError) status = 404;
-      if(error instanceof NotValidError) status = 422;
+      if (error instanceof NotFoundError) status = 404;
+      if (error instanceof NotValidError) status = 422;
 
-      res.status(status).send({error: error.message});
+      res.status(status).send({ error: error.message });
     }
   }
 
@@ -31,30 +31,73 @@ export class EmployeeController{
    * @Route POST /employee/
    * @returns  Employee created
   */
-  public static async create(req: Request, res: Response){
-    const {employee} = req.body;
-    
+  public static async create(req: Request, res: Response) {
+    const { employee } = req.body;
+
     try {
       const newEmployee = await new EmployeeRepository(req.app.get('db')).create(employee);
       res.status(200).send(newEmployee);
     } catch (error) {
       let status = 400;
-      if(error instanceof NoDataError) status;
-      if(error instanceof NotValidError) status = 422;
+      if (error instanceof NoDataError) status;
+      if (error instanceof NotValidError) status = 422;
 
-      res.status(status).send({error: error.message});
+      res.status(status).send({ error: error.message });
     }
   }
 
-  public static async read(req: Request, res: Response){
-    res.status(400).send({error: 'Not implemented'});
+  /**
+   * Read all employees
+   * @Route GET /employee/
+   * @returns Array of employees
+   */
+  public static async read(req: Request, res: Response) {
+    try {
+      const employees = await new EmployeeRepository(req.app.get('db')).findAll();
+      res.status(200).send(employees);
+    } catch (error) {
+      res.status(400).send({ error: error.message });
+    }
   }
 
-  public static async update(req: Request, res: Response){
-    res.status(400).send({error: 'Not implemented'});
+  /**
+   * Update an employee
+   * @Route PUT /employee/:id
+   * @returns  Employee updated
+   */
+  public static async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { employee } = req.body;
+
+    try {
+      const updatedEmployee = await new EmployeeRepository(req.app.get('db')).update(id, employee);
+      res.status(200).send(updatedEmployee);
+    } catch (error) {
+      let status = 400;
+      if (error instanceof NotFoundError) status = 404;
+      if (error instanceof NotValidError) status = 422;
+
+      res.status(status).send({ error: error.message });
+    }
   }
 
-  public static async delete(req: Request, res: Response){
-    res.status(400).send({error: 'Not implemented'});
+  /**
+   * Delete an employee by id
+   * @Route DELETE /employee/:id
+   * @returns Employee deleted
+   */
+  public static async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const deletedEmployee = await new EmployeeRepository(req.app.get('db')).delete(id);
+      res.status(200).send(deletedEmployee);
+    } catch (error) {
+      let status = 400;
+      if (error instanceof NotFoundError) status = 404;
+      if (error instanceof NotValidError) status = 422;
+
+      res.status(status).send({ error: error.message });
+    }
   }
 }
--- src/models/repositories/EmployeeRepository.ts
+++ src/models/repositories/EmployeeRepository.ts
@@ -6,9 +6,9 @@ import { NotValidError } from '../../errors/NotValidError';
 import { Employee } from '../Employee';
 import { IRepository } from './IRepository';
 
-export class EmployeeRepository implements IRepository{
+export class EmployeeRepository implements IRepository {
   private _db: JsonDB;
-  constructor(db: JsonDB){
+  constructor(db: JsonDB) {
     this._db = db;
   }
   public get db(): JsonDB {
@@ -23,12 +23,13 @@ export class EmployeeRepository implements IRepository{
    * Create an employee with passed data
    * @param data Employee
    * @returns Employee created as a Promise
+   * @throws NoDataError if data is not passed, NotValidError if data is not valid
    */
   public async create(data: Employee): Promise<any> {
     const employeeData = data as Employee;
 
-    if(!data) throw new NoDataError('Employee is required');
-    if(!this.validate(employeeData)) throw new NotValidError('Employee is not valid');
+    if (!data) throw new NoDataError('Employee is required');
+    if (!this.validate(employeeData)) throw new NotValidError('Employee is not valid');
 
     employeeData.registration = this.generateRegistration();
     await this.db.push('/employees[]', employeeData, true);
@@ -48,40 +49,95 @@ export class EmployeeRepository implements IRepository{
    * @param employeeData 
    * @returns true if employee data is valid, false otherwise
    */
-  private validate(employeeData: Employee):boolean {
-    if(!employeeData.name) return false;
-    if(!employeeData.email) return false;
-    if(!employeeData.password) return false;
-    if(!employeeData.age) return false;
-    if(!employeeData.role) return false;
-    if(!employeeData.cpf) return false;
+  private validate(employeeData: Employee): boolean {
+    if (!employeeData.name) return false;
+    if (!employeeData.email) return false;
+    if (!employeeData.password) return false;
+    if (!employeeData.age) return false;
+    if (!employeeData.role) return false;
+    if (!employeeData.cpf) return false;
 
     return true;
   }
 
+  /**
+   * Find one employee by id
+   * @param id
+   * @returns Employee as a Promise
+   * @throws NotFoundError if employee not found, NotValidError if id is not valid
+   */
   public async findOne(id: string): Promise<any> {
     const validId = id.match(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
-    if(!validId) throw new NotValidError('Id is not valid');
+    if (!validId) throw new NotValidError('Id is not valid');
 
-    try{
+    try {
       const employeeIndex = await this.db.getIndex('/employees', id, 'registration');
       const employee = await this.db.getData(`/employees[${employeeIndex}]`);
 
-      if(employeeIndex === -1) throw new NoDataError('Employee not found');
+      if (employeeIndex === -1) throw new NotFoundError('Employee not found');
       return employee;
-    }catch(error){
+    } catch (error) {
       throw new NotFoundError('Employee not found');
     }
   }
 
-
+  /**
+   * Get all employees from database
+   * @returns all employees as a Promise
+   */
   public async findAll(): Promise<any> {
-    throw new Error('Method not implemented.');
+    const employees = await this.db.getData('/employees');
+    return employees;
   }
+  
+  /**
+   * 
+   * @param id of employee to update
+   * @param data Employeeto update
+   * @returns Employee updated as a Promise
+   */
   public async update(id: string, data: Employee): Promise<any> {
-    throw new Error('Method not implemented.');
+    const employeeData = data as Employee;
+
+    const validId = id.match(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
+    
+    if (!validId) throw new NotValidError('Id is not valid');
+    if (!data) throw new NoDataError('Employee is required');
+    if (!this.validate(employeeData)) throw new NotValidError('Employee is not valid');
+
+    try{
+      const employeeIndex = await this.db.getIndex('/employees', id, 'registration');
+
+      if (employeeIndex === -1) throw new NotFoundError('Employee not found');
+
+      await this.db.push(`/employees[${employeeIndex}]`, employeeData, true);
+      
+      return employeeData;
+    }catch(error){
+      throw new NotFoundError('Employee not found');
+    }
+
   }
+
+  /**
+   * Delete an employee by id
+   * @param id of employee to delete
+   * @returns Employee deleted as a Promise
+   */
   public async delete(id: string): Promise<any> {
-    throw new Error('Method not implemented.');
+    const validId = id.match(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
+    if (!validId) throw new NotValidError('Id is not valid');
+
+    try {
+      const employeeIndex = await this.db.getIndex('/employees', id, 'registration');
+      const employee = await this.db.getData(`/employees[${employeeIndex}]`);
+
+      if (employeeIndex === -1) throw new NotFoundError('Employee not found');
+
+      await this.db.delete(`/employees[${employeeIndex}]`);
+      return employee;
+    } catch (error) {
+      throw new NotFoundError('Employee not found');
+    }
   }
 }
