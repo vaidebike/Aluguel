52b0f66b434a56e882fc21736d19b218b5878c4a
cassianojr <cassiano.jr.2009@hotmail.com>
2022-12-13 15:32:37 -0300

    fix: separate unit and integration tests by folders

--- src/tests/integration/app.test.ts
+++ src/tests/integration/app.test.ts
@@ -1,14 +1,16 @@
 import request from 'supertest';
-import server from '../app';
+import server from '../../app';
 describe('Get Endpoint /', () => {
+
+  
   it('Get /', async () => {
-    const res =  await  request(server)
+    const res = await request(server)
       .get('/');
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('name');
     expect(res.body).toHaveProperty('version');
   });
-  afterEach(done  => {
+  afterEach(done => {
     // close server conection
     server.close();
     done();
